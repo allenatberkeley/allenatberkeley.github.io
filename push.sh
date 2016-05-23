cf0a4bba09d8e043291d82d96e2a5d005e4d54e8
zealng <zealng@berkeley.edu>
2016-05-22 23:58:21 -0700

    versioning

--- push.sh
+++ push.sh
@@ -1,6 +1,4 @@
-# pushes to "origin master"
-# you might need to modify that ^ on line 20
-# adds -all
+# syntax check
 if [[ $# -eq 0 ]] ; then
     echo 'Pass in a message for the commit!:'
     echo 'Ex) -- (No Quotes Needed)'
@@ -13,9 +11,18 @@ for i in "$@"; do
     C="$C ${i//\"/\\\"}"
 done
 
+#increments version number
+VERSION="version.txt"
+count='cat $VERSION'
+((count++))
+echo $count > $VERSION
+
 set -e
+# adds --all
 git add --all
 # git status
 git commit -m "$C"
+# pushes to "origin master"
+# you might need to modify the following line
 git push origin HEAD:master
 # git status
