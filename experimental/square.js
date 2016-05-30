2f5a6a01029d1848a3e01c35778ada87aad1e6c7
zealng <zealng@berkeley.edu>
2016-05-29 20:39:14 -0700

    square test

--- experimental/square.js
+++ experimental/square.js
@@ -12,18 +12,11 @@ var square = document.querySelector('.square')
 // };
 
 var scroll_pos = 0;
-var prev_pos = 0;
 var ticking = false;
 
 function squareBounce(scroll_pos) {
-    // var y_val = 100;
-
-    // if (scroll_pos < prev_pos) {
-    //     y_val *= -1;
-    // }
-
     dynamics.animate(square, {
-        translateY: 100;
+        translateY: 100
     }, {
         type: dynamics.bounce,
         duration: 1700,
@@ -36,8 +29,8 @@ window.addEventListener('scroll', function(e) {
   scroll_pos = window.scrollY;
   if (!ticking) {
     window.requestAnimationFrame(function() {
-        squareBounce(scroll_pos);
-        ticking = false;
+      squareBounce(scroll_pos);
+      ticking = false;
     });
   }
   ticking = true;
