1665305c33e97cd34b0a2de114240b9d4c295d3a
zealng <zealng@berkeley.edu>
2016-05-29 21:09:32 -0700

    leaf set up

---
+++ experimental/leaf.js
@@ -0,0 +1,50 @@
+var leaf = document.querySelector('.leaf')
+
+// window.onscroll = function() {
+//     dynamics.animate(leaf, {
+//         translateY: 100
+//     }, {
+//         type: dynamics.bounce,
+//         duration: 1700,
+//         frequency: 42,
+//         friction: 100,
+//     });
+// };
+
+var scroll_pos = 0;
+var prev_pos = 0;
+var ticking = false;
+
+function leafBounce(scroll_pos) {
+
+    // console.log("scroll: " + scroll_pos);
+    // console.log("prev: " + prev_pos);
+
+    var y_val = 100;
+
+    if (scroll_pos < prev_pos) {
+        y_val *= -1;
+    }
+
+    dynamics.animate(leaf, {
+        translateY: y_val
+    }, {
+        type: dynamics.bounce,
+        duration: 1700,
+        frequency: 42,
+        friction: 100
+    });
+
+    prev_pos = scroll_pos;
+};
+
+window.addEventListener('scroll', function(e) {
+  scroll_pos = window.scrollY;
+  if (!ticking) {
+    window.requestAnimationFrame(function() {
+        leafBounce(scroll_pos);
+        ticking = false;
+    });
+  }
+  ticking = true;
+});
