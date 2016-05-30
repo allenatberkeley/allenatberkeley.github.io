d46c0b30ad7b770e5420f3174a46d10ca8dadfd8
zealng <zealng@berkeley.edu>
2016-05-29 20:51:54 -0700

    square test directional 2

--- experimental/square.js
+++ experimental/square.js
@@ -17,14 +17,16 @@ var ticking = false;
 
 function squareBounce(scroll_pos) {
 
-    console.log("scroll: " + scroll_pos);
-    console.log("prev: " + prev_pos);
+    // console.log("scroll: " + scroll_pos);
+    // console.log("prev: " + prev_pos);
 
-    var y_val = 100;
+    // var y_val = 100;
 
-    if (scroll_pos < prev_pos) {
-        y_val *= -1;
-    }
+    // if (scroll_pos < prev_pos) {
+    //     y_val *= -1;
+    // }
+
+    var y_val = scroll_pos - prev_pos;
 
     dynamics.animate(square, {
         translateY: y_val
