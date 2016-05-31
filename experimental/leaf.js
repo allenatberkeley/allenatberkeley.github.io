3fa7e516e7cce13c971bd44464807385b7a582e4
zealng <zealng@berkeley.edu>
2016-05-31 14:13:15 -0700

    leaf double scroll

--- experimental/leaf.js
+++ experimental/leaf.js
@@ -11,22 +11,30 @@ var leaf = document.querySelector('.leaf')
 //     });
 // };
 
-var scroll_pos = 0;
-var prev_pos = 0;
+var scroll_pos_x = 0;
+var scroll_pos_y = 0;
+var prev_pos_x = 0;
+var prev_pos_y = 0;
 var ticking = false;
 
-function leafBounce(scroll_pos) {
+function leafBounce() {
 
     // console.log("scroll: " + scroll_pos);
     // console.log("prev: " + prev_pos);
 
+    var x_val = 100;
     var y_val = 100;
 
-    if (scroll_pos < prev_pos) {
+    if (scroll_pos_x < prev_pos_x) {
+        x_val *= -1;
+    }
+    if (scroll_pos_y < prev_pos_y) {
         y_val *= -1;
     }
 
+
     dynamics.animate(leaf, {
+        translateX: x_val
         translateY: y_val
     }, {
         type: dynamics.bounce,
@@ -35,32 +43,34 @@ function leafBounce(scroll_pos) {
         friction: 100
     });
 
-    prev_pos = scroll_pos;
+    prev_pos_x = scroll_pos_x;
+    prev_pos_y = scroll_pos_y;
 };
 
 window.addEventListener('scroll', function(e) {
-  scroll_pos = window.scrollY;
-  if (!ticking) {
-    window.requestAnimationFrame(function() {
-        leafBounce(scroll_pos);
-        squareBounce(scroll_pos);
+    scroll_pos_x = window.scrollX;
+    scroll_pos_y = window.scrollY;
+    if (!ticking) {
+        window.requestAnimationFrame(function() {
+        leafBounce();
+        squareBounce();
         ticking = false;
-    });
-  }
-  ticking = true;
+        });
+    }
+    ticking = true;
 });
 
 /* SQUARE EXAMPLE FIX */
 var square = document.querySelector('.square')
 var prev_posS = 0;
-function squareBounce(scroll_pos) {
+function squareBounce() {
 
-    // console.log("scroll: " + scroll_pos);
-    // console.log("prev: " + prev_pos);
+    // console.log("scroll: " + scroll_pos_x);
+    // console.log("prev: " + prev_pos_x);
 
     var y_val = 100;
 
-    if (scroll_pos < prev_posS) {
+    if (scroll_pos_y < prev_posS) {
         y_val *= -1;
     }
 
@@ -73,5 +83,5 @@ function squareBounce(scroll_pos) {
         friction: 100
     });
 
-    prev_posS = scroll_pos;
+    prev_posS = scroll_pos_y;
 };
